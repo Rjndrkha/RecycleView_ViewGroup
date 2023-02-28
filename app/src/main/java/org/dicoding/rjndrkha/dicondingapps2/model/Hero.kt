132ac39b2893762b5fd6b3391a805a52dff94754
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2023-02-28 20:57:18 +0700

    Done ALL

---
+++ app/src/main/java/org/dicoding/rjndrkha/dicondingapps2/model/Hero.kt
@@ -0,0 +1,19 @@
+package org.dicoding.rjndrkha.dicondingapps2.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+//@Parcelize
+//data class Hero(
+//    val name: String,
+//    val description: String,
+//    val photo: Int
+//) : Parcelable
+
+//with glide
+@Parcelize
+data class Hero(
+    var name: String,
+    var description: String,
+    var photo: String
+) : Parcelable
